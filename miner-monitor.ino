e7e44ddb6e0dc8cf89b83e74321af0aa2341ac22
Peter Jin <peter.zjin@gmail.com>
2018-02-11 22:34:13 +0800

    Add tasks for WiFi and Miner info
    Signed-off-by: Peter Jin <peter.zjin@gmail.com>

--- miner-monitor.ino
+++ miner-monitor.ino
@@ -16,7 +16,6 @@
 #include <SPI.h>
 #include <U8g2lib.h>
 #include <U8x8lib.h>
-#include <Adafruit_INA219.h>
 #include <DallasTemperature.h>
 #include <Wire.h>
 #include <pcf8574_esp.h>
@@ -43,7 +42,6 @@
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-Adafruit_INA219 INA219;
 PCF857x pcf8574(0x27, &Wire);
 
 //U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, D5, D7, D8);
@@ -56,16 +54,59 @@ typedef struct {
   int interval;
   timer_cb callback;
 } TimerTask;
+void update_wifi_status();
 void update_temp();
 void update_tacho_pwm();
 void update_disp();
+void update_miners();
 TimerTask tasks[] = {
+  {-1,  500, update_wifi_status},
   {-1, 2000, update_temp},
   {-1, 2000, update_tacho_pwm},
   {-1, 2000, update_disp},
+  {-1, 9999, update_miners},
 };
 #define TASKS_NUM (sizeof(tasks) / sizeof(TimerTask))
 Timer task_list;
+
+#define MAX_GPU_PER_MINER 6
+enum miner_type {
+  CLAYMORE_ETH,
+  CLAYMORE_ZEC,
+  ZM_ZEC,
+};
+typedef struct {
+  char *ip_host;
+  int  port;
+  enum miner_type type;
+} Miner;
+Miner miners[] = {
+  {.ip_host = "peter-m1", .port = 1111, .type = CLAYMORE_ETH},
+  {.ip_host = "peter-pc", .port = 2222, .type = CLAYMORE_ZEC},
+  {.ip_host = "peter-pc", .port = 4444, .type = ZM_ZEC},
+};
+#define MINERS_NUM (sizeof(miners) / sizeof(Miner))
+typedef struct {
+  int gpu_num;
+  int uptime;
+  int t_hash;
+  int t_accepted_s;
+  int t_rejected_s;
+  int t_invalid_s;
+  int pool_switch;
+  int hash[MAX_GPU_PER_MINER];
+  int accepted_s[MAX_GPU_PER_MINER];
+  int rejected_s[MAX_GPU_PER_MINER];
+  int invalid_s[MAX_GPU_PER_MINER];
+  int temp[MAX_GPU_PER_MINER];
+  int fan[MAX_GPU_PER_MINER];
+} Miner_s;
+Miner_s miners_s[MINERS_NUM];
+
+unsigned int system_status;
+
+#define SYS_WIFI_CONN_BIT   0
+#define SYS_WIFI_CONNECTED  bitRead(system_status, SYS_WIFI_CONN_BIT)
 
 float temp_val[TEMP_SENSOR_NO];
 int tacho_pwm_addr[TACHO_PWM_NO] = {TACHO_PWM_ADDR1, TACHO_PWM_ADDR2};
@@ -85,7 +126,6 @@ void setup() {
   pinMode(ONE_WIRE_BUS, INPUT_PULLUP);
   sensors.begin();
   sensors.setWaitForConversion(FALSE);
-  INA219.begin();
   u8g2.begin();
 
   for (i = 0; i < TASKS_NUM; i++) {
@@ -97,45 +137,13 @@ void loop() {
   String str;
   int input_pwm_val = 0;
   WiFiClient client;
-  DynamicJsonBuffer jsonBuffer1, jsonBuffer2;
 
-/*
+  /*
   delay(1000);
   Serial.println("TEST");
   return;
   */
 
-/*
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Connecting to ");
-    Serial.print(HOME_SSID);
-    Serial.println("...");
-    WiFi.begin(HOME_SSID, HOME_PASSWD);
-
-    if (WiFi.waitForConnectResult() != WL_CONNECTED)
-      goto out;
-
-    Serial.println("WiFi connected");
-  }
-
-  if (client.connect("192.168.2.216", 1111)) {
-    client.println("{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"miner_getstat1\"}");
-    String line = client.readStringUntil('\r');
-    Serial.println(line);
-    client.stop();
-    JsonObject& root = jsonBuffer1.parseObject(line);
-    int uptime = root["result"][1];
-    Serial.println(uptime);
-    //JsonArray& hashrate = root["result"][3];
-    String str = root["result"][3];
-    Serial.println(str);
-    str.replace(';', ',');
-    JsonArray& hashrate = jsonBuffer2.parseArray("["+str+"]");
-    Serial.println(hashrate.size());
-  } else {
-    Serial.println("Cannot connect to peter-m1");
-  }
-*/
 
   /*
   Serial.println(WiFi.localIP());
@@ -182,6 +190,28 @@ void loop() {
 
 }
 
+/* The task to update the wifi status */
+void update_wifi_status() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connecting to ");
+    Serial.print(HOME_SSID);
+    Serial.println("...");
+    WiFi.begin(HOME_SSID, HOME_PASSWD);
+
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      bitClear(system_status, SYS_WIFI_CONN_BIT);
+      return;
+    }
+
+    bitSet(system_status, SYS_WIFI_CONN_BIT);
+
+    Serial.println("WiFi connected");
+  } else {
+    bitSet(system_status, SYS_WIFI_CONN_BIT);
+  }
+}
+
+/* The task to get the temperature from sensors */
 void update_temp() {
   int i;
 
@@ -195,6 +225,7 @@ void update_temp() {
   Serial.println();
 }
 
+/* The task to get the tacho and pwm values from tachometers */
 void update_tacho_pwm() {
   int i, j;
   unsigned char val_h, val_l;
@@ -218,6 +249,7 @@ void update_tacho_pwm() {
   }
 }
 
+/* The task to draw the display */
 void update_disp() {
   #if 0
   u8g2.firstPage();
@@ -249,6 +281,97 @@ void update_disp() {
   u8g2.sendBuffer();
   #endif
   curr_tacho_pwm = !curr_tacho_pwm;
+}
+
+/* The task to get the info from miners */
+void update_miners() {
+  String str;
+  WiFiClient client;
+  DynamicJsonBuffer json_buf;
+  int i, j, tmp_array[MAX_GPU_PER_MINER * 2];
+
+  if (!SYS_WIFI_CONNECTED) {
+    return;
+  }
+
+  for (i = 0; i < MINERS_NUM; i++) {
+    if (!client.connect(miners[i].ip_host, miners[i].port))
+      break;
+
+    switch (miners[i].type) {
+      case CLAYMORE_ETH:
+      case CLAYMORE_ZEC:
+        {
+          if (miners[i].type == CLAYMORE_ETH)
+            client.println(
+              "{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"miner_getstat2\"}");
+          else
+            client.println(
+              "{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"miner_getstat1\"}");
+          str = client.readStringUntil('}') + '}';
+          client.stop();
+          JsonArray& result = (json_buf.parseObject(str))["result"];
+          miners_s[i].uptime = result[1];
+          str2array(result[2], ';', tmp_array, 3);
+          miners_s[i].t_hash = tmp_array[0];
+          miners_s[i].t_accepted_s = tmp_array[1];
+          miners_s[i].t_rejected_s = tmp_array[2];
+          //Serial.println(result.get<String>(2));
+          miners_s[i].gpu_num = str2array(result[3], ';',
+              miners_s[i].hash, MAX_GPU_PER_MINER);
+          str2array(result[6], ';', tmp_array, MAX_GPU_PER_MINER * 2);
+          for (j = 0; j < miners_s[i].gpu_num; j++) {
+            miners_s[i].temp[j] = tmp_array[j * 2];
+            miners_s[i].fan[j] = tmp_array[j * 2 + 1];
+          }
+          str2array(result[8], ';', tmp_array, MAX_GPU_PER_MINER * 2);
+          miners_s[i].t_invalid_s = tmp_array[0];
+          miners_s[i].pool_switch = tmp_array[1];
+          str2array(result[9], ';', miners_s[i].accepted_s, MAX_GPU_PER_MINER);
+          str2array(result[10], ';', miners_s[i].rejected_s, MAX_GPU_PER_MINER);
+          str2array(result[11], ';', miners_s[i].invalid_s, MAX_GPU_PER_MINER);
+        }
+        break;
+      case ZM_ZEC:
+        {
+          client.println("{\"id\":1, \"method\":\"getstat\"}");
+          str = client.readStringUntil('}') + '}';
+          str += client.readStringUntil('}') + '}';
+          client.stop();
+          //Serial.println(str);
+          JsonObject& root = json_buf.parseObject(str);
+          miners_s[i].uptime = ((int)root["uptime"]) / 60;
+          JsonArray& result = root["result"];
+          miners_s[i].gpu_num = result.size();
+          miners_s[i].t_hash = 0;
+          miners_s[i].t_accepted_s = 0;
+          miners_s[i].t_rejected_s = 0;
+          for (j = 0; j < miners_s[i].gpu_num; j++) {
+            miners_s[i].temp[j] = result[j]["temperature"];
+            miners_s[i].hash[j] = result[j]["sol_ps"];
+            miners_s[i].t_hash += miners_s[i].hash[j];
+            miners_s[i].accepted_s[j] = result[j]["accepted_shares"];
+            miners_s[i].t_accepted_s += miners_s[i].accepted_s[j];
+            miners_s[i].rejected_s[j] = result[j]["rejected_shares"];
+            miners_s[i].t_rejected_s += miners_s[i].rejected_s[j];
+          }
+        }
+        break;
+      default:
+        break;
+    }
+    str = String(miners[i].ip_host) + ": " + miners_s[i].uptime / 60 + "H"
+          + miners_s[i].uptime % 60 + "M "+ miners_s[i].gpu_num + " GPUS "
+          + miners_s[i].t_hash + " Mh/s (" + miners_s[i].t_accepted_s + " "
+          + miners_s[i].t_rejected_s + " " + miners_s[i].t_invalid_s + ")";
+    Serial.println(str);
+    for (j = 0; j < miners_s[i].gpu_num; j++) {
+      str = String("  ") + j + ": " + miners_s[i].hash[j] + " Mh/s "
+            + miners_s[i].temp[j] + "C (" + miners_s[i].accepted_s[j] + " "
+            + miners_s[i].rejected_s[j] + " " + miners_s[i].invalid_s[j] + ")";
+      Serial.println(str);
+    }
+  }
 }
 
 String get_str_int(int *buf, int sz, int offset, int width) {
@@ -296,4 +419,25 @@ String get_str_float(float *buf, int sz, int offset, int width) {
   }
 
   return all_str;
+}
+
+int str2array(String str, char sep, int *array, int max_size) {
+  int p1 = 0, p2 = 0, i = 0, len = str.length();
+
+  while (1) {
+    if (-1 == (p2 = str.indexOf(sep, p1))) {
+      array[i++] = str.substring(p1).toInt();
+      break;
+    }
+
+    if (p1 != p2)
+      array[i++] = str.substring(p1, p2).toInt();
+
+    if (i >= max_size)
+      break;
+
+    p1 = p2 + 1;
+  }
+
+  return i;
 }
